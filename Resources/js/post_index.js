f0da00a533401216a56887be9e2928e5f4922fb2
Melvin Marte <melmarte@hotmail.com>
2017-12-10 13:34:38 -0400

    post_index

--- Resources/js/post_index.js
+++ Resources/js/post_index.js
@@ -7,20 +7,30 @@ function cargarPost() {
         url: root + '/posts',
         method: 'GET'
     }).then(function (data) {
-        
+
         var localStore = window.localStorage;
         var postFavorito = {};
         var dbPostFavorito = localStorage.getItem('PostFavorito');
-        if (dbPostFavorito !=null){
-            postFavorito = JSON.parse(dbPostFavorito);  
+        if (dbPostFavorito != null) {
+            postFavorito = JSON.parse(dbPostFavorito);
         }
-        
+
 
 
         //recorrer el areglo de los post
         $.each(data, function (i, p) {
+            var localStorage = window.localStorage;
+            var postFavorito = {};
 
-            var existe=p.id in postFavorito;
+            var dbPostFavorito = localStorage.getItem('postFavorito');
+
+            if (dbPostFavorito != null) {
+                postFavorito = JSON.parse(dbPostFavorito);
+
+
+
+            }
+            var existe = p.id in postFavorito;
 
             var post =
                 '<div class="row">' +
@@ -28,7 +38,7 @@ function cargarPost() {
                 '               <h3>' + p.title + '</h3>' +
                 '           </div>' +
                 '      <div class="col-md-2">' +
-                '           <button class="post_boton btn btn-primary glyphicon glyphicon-star-empty" data-post-id="' + p.id + '"></button>' +
+                '           <button class="post_boton btn btn-primary glyphicon ' + (existe ? 'glyphicon-star' : 'glyphicon-star-empty') + '" data-post-id="' + p.id + '"></button>' +
                 '       </div>' +
                 '     </div>' +
                 '  </div>' +
@@ -54,14 +64,14 @@ function cargarPost() {
             $('#post').append(post);
 
         });
-        
+
         $(".post_boton").click(function () {
-            var postId=$(this).data('post-id');
-            var existe=agregarPostFavorito(postId);
-            
-            $(this).removeClass(existe ? 'glyphicon-star-empty':'glyphicon-star');
-            $(this).addClass(existe ?'glyphicon-star':'glyphicon-star-empty');
-            
+            var postId = $(this).data('post-id');
+            var existe = agregarPostFavorito(postId);
+
+            $(this).removeClass(existe ? 'glyphicon-star-empty' : 'glyphicon-star');
+            $(this).addClass(existe ? 'glyphicon-star' : 'glyphicon-star-empty');
+
             // if (existe){
             //     $(this).removeClass('glyphicon-star-empty');
             //     $(this).addClass('glyphicon-star');
@@ -76,28 +86,29 @@ function cargarPost() {
 };
 
 
-function agregarPostFavorito (postId){
-    var localStore = window.localStorage;
+function agregarPostFavorito(postId) {
+    var localStorage = window.localStorage;
     var postFavorito = {};
 
-    var dbPostFavorito = localStorage.getItem('PostFavorito');
-    
-    if (postFavorito !=null){
+    var dbPostFavorito = localStorage.getItem('postFavorito');
+
+    if (dbPostFavorito != null) {
         postFavorito = JSON.parse(dbPostFavorito);
-        
-        
+
+
         var existe = false;
-       if (postId in postFavorito){
-           
+        if (postId in postFavorito) {
+            delete postFavorito[postId];
         }
-        else{
+        else {
             existe = true;
             postFavorito[postId] = true
         }
-        
-        localStorage.setItem('postFavorito', JSON.stringify(postFavorito));
-        return existe;
+
+
     }
+    localStorage.setItem('postFavorito', JSON.stringify(postFavorito));
+    return existe;
 };
 
 $(document).ready(function () {
